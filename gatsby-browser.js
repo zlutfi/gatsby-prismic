afa175421635532af2d66e3324ff402233d4856e
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-12 12:19:01 -0400

    Added support for service worker update notice

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+// Display a message when a service worker updates
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
