64da979e4777cfcf1fdcff55f06b408b1a8fa905
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-12 12:19:31 -0400

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,61 @@
+// Configure environments & security token
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// Import external utilities
+const prismicHtmlSerializer = require("./src/utils/htmlSerializer")
+const prismicLinkResolver = require("./src/utils/linkResolver")
+
+// Added Robots.txt configuration for Netlify deploy previews
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://www.zlutfi.com",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === "production"
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter with Prismic`,
+    // Add netlify site url variable
+    siteUrl: NETLIFY_SITE_URL,
+    description: `Kick off your next, great Gatsby project with this starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `zlutfi.com`,
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        resolveEnv: () => process.env.GATSBY_ENV,
+        env: {
+          development: {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+          },
+          production: {
+            policy: [{ userAgent: "*", allow: "/" }],
+          },
+        },
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*" }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -13,6 +64,46 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: ["/category/*", `/path/to/page`, "404", "/success", "/blog/*"],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          }),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `${process.env.GA_TRACKING_ID}`,
+      },
+    },
+    `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,8 +118,62 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from prismic.io.
+     */
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        // The name of your prismic.io repository. This is required.
+        repositoryName: `${process.env.PRISMIC_REPO}`,
+
+        // An API access token to your prismic.io repository. This is required.
+        accessToken: `${process.env.PRISMIC_API_KEY}`,
+
+        // Set a link resolver function used to process links in your content.
+        linkResolver: () => prismicLinkResolver,
+        // Set a list of links to fetch and be made available in your link
+        // resolver function.
+        // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
+        fetchLinks: [
+          // Your list of links
+        ],
+
+        // Set an HTML serializer function used to process formatted content.
+        htmlSerializer: () => prismicHtmlSerializer,
+
+        // Provide an object of Prismic custom type JSON schemas to load into
+        schemas: {
+          // Your custom types mapped to schemas
+          // project: require("./src/schemas/project.json"),
+          // category: require("./src/schemas/category.json"),
+          // homepage: require("./src/schemas/homepage.json"),
+          // menu: require("./src/schemas/menu.json"),
+          // page: require("./src/schemas/page.json"),
+          // post: require("./src/schemas/post.json"),
+        },
+
+        // Set a default language when fetching documents. The default value is
+        // '*' which will fetch all languages.
+        // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
+        lang: "*",
+
+        // Set a function to determine if images are downloaded locally and made
+        // available for gatsby-transformer-sharp for use with gatsby-image.
+        shouldNormalizeImage: ({ node, key, value }) => {
+          // Return true to normalize the image or false to skip.
+          return true
+        },
+        // Set the prefix for the filename where type paths for your schemas are
+        // stored.
+        typePathsFilenamePrefix: "prismic-typepaths---zlutfi",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    "gatsby-plugin-netlify-cache",
   ],
 }
