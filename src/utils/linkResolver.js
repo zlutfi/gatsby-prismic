6facb13055407d87571c4a5fa68cd92c30929eac
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-12 12:17:58 -0400

    Adding link resolver function

---
+++ src/utils/linkResolver.js
@@ -0,0 +1,31 @@
+// In src/prismic-configuration.js
+const linkResolver = doc => {
+  // URL for a homepage type
+  // if (doc.type === "homepage") {
+  //   return "/"
+  // }
+  // // URL for a post type
+  // if (doc.type === "post") {
+  //   return `/blog/${doc.uid}`
+  // }
+  // // URL for a project type
+  // if (doc.type === "project") {
+  //   return `/projects/${doc.uid}`
+  // }
+  // // URL for a category type
+  // if (doc.type === "category") {
+  //   return `/projects/category/${doc.uid}`
+  // }
+  // // URL for a page type
+  // if (doc.type === "page") {
+  //   return `/${doc.uid}`
+  // }
+  // // URL for a static page type
+  // if (doc.type === "static_page") {
+  //   return `/${doc.uid}`
+  // }
+
+  // Backup for all other types
+  return "/"
+}
+module.exports = linkResolver
