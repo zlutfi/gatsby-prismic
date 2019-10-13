e06cc3848fbb33a8185bda01a072aaf8735ad59f
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-10-13 11:06:21 -0400

    Migrated inline styles to stylesheet

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div className="inner">
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
